fd5db8b57809cac22179c402b5736196c59cb4dc
Evgenija26 <98258381+Evgenija26@users.noreply.github.com>
2022-12-07 10:37:00 +0200

    hw commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { GlobalStyle } from './GlobalStyle';
+import { Component } from 'react';
+
+
+
+export class App extends Component {
+
+  state = {
+  contacts: [],
+  name: ''
+  }
+  
+  render() {
+    
+    return (
+
+      <>
+        <h1>Evgenija</h1>
+        <GlobalStyle />
+      </>
+    );
+  
+  // 
+  //   <div style={{
+  //       height: '100vh',
+  //       display: 'flex',
+  //       justifyContent: 'center',
+  //       alignItems: 'center',
+  //       fontSize: 40,
+  //       color: '#010101'
+  //     }}>
+  //     Evgenija
+  //   </div>
+  // );
+  };
 };
+
+
